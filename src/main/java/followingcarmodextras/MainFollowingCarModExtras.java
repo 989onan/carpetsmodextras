73e17742b4638119ca4d9817ed99fda8fb7eb79a
989onan <onanchew@gmail.com>
2021-12-01 14:16:30 -0500

    1.3.0
    -fixed siding to be client side only in mods inf folder and in code.
    
    -last but not least, update to 1.18.

--- src/main/java/followingcarmodextras/MainFollowingCarModExtras.java
+++ src/main/java/followingcarmodextras/MainFollowingCarModExtras.java
@@ -1,7 +1,10 @@
 package followingcarmodextras;
 
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
@@ -13,6 +16,10 @@ public class MainFollowingCarModExtras
     public static final String MODID = "followingcarmodextras";
 
     public MainFollowingCarModExtras() {
+    	if (FMLEnvironment.dist != Dist.CLIENT) {
+    		LOGGER.info("Disabled because not running on the client.");
+			return;
+		}
     	LOGGER.info("Car Pets Mod Extras loaded! Let's have some cool models and epic fun!");
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
